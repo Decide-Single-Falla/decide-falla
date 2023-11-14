b5a1aab7bccdee1487a476728b780fff3eb7b0d3
pabquide <pabloquindosdelariva1b@gmail.com>
2023-11-14 17:50:02 +0100

    feat: Modify views.py, models.py and serializers.py for anonymous voting
    Changes the backend and logic to start the implementation of anonymous voting feature.

--- decide/store/views.py
+++ decide/store/views.py
@@ -44,39 +44,58 @@ class StoreView(generics.ListAPIView):
         if not_started or is_closed:
             #print("por aqui 42")
             return Response({}, status=status.HTTP_401_UNAUTHORIZED)
-
-        uid = request.data.get('voter')
         vote = request.data.get('vote')
 
-        if not vid or not uid or not vote:
-            return Response({}, status=status.HTTP_400_BAD_REQUEST)
+        if not vid or not vote:
+                return Response({}, status=status.HTTP_400_BAD_REQUEST)
 
-        # validating voter
-        if request.auth:
+        if(voting[0].get('private')):
+
             token = request.auth.key
+
+            voter = mods.post('authentication', entry_point='/getuser/', json={'token': token})
+
+            uid = request.data.get('voter')
+            
+            a = vote.get("a")
+            b = vote.get("b")
+
+            v = Vote.objects.create(voting_id=vid, voter_id=uid)
+
         else:
-            token = "NO-AUTH-VOTE"
-        voter = mods.post('authentication', entry_point='/getuser/', json={'token': token})
-        voter_id = voter.get('id', None)
-        if not voter_id or voter_id != uid:
-            # print("por aqui 59")
-            return Response({}, status=status.HTTP_401_UNAUTHORIZED)
+            uid = request.data.get('voter')
 
-        # the user is in the census
-        perms = mods.get('census/{}'.format(vid), params={'voter_id': uid}, response=True)
-        if perms.status_code == 401:
-            # print("por aqui 65")
-            return Response({}, status=status.HTTP_401_UNAUTHORIZED)
+            if not uid:
+                return Response({}, status=status.HTTP_400_BAD_REQUEST)
 
-        a = vote.get("a")
-        b = vote.get("b")
+            # validating voter
+            if request.auth:
+                token = request.auth.key
+            else:
+                token = "NO-AUTH-VOTE"
+            voter = mods.post('authentication', entry_point='/getuser/', json={'token': token})
+            voter_id = voter.get('id', None)
+            if not voter_id or voter_id != uid:
+                # print("por aqui 59")
+                return Response({}, status=status.HTTP_401_UNAUTHORIZED)
 
-        defs = { "a": a, "b": b }
-        v, _ = Vote.objects.get_or_create(voting_id=vid, voter_id=uid,
+            # the user is in the census
+            perms = mods.get('census/{}'.format(vid), params={'voter_id': uid}, response=True)
+            if perms.status_code == 401:
+                # print("por aqui 65")
+                return Response({}, status=status.HTTP_401_UNAUTHORIZED)
+
+            a = vote.get("a")
+            b = vote.get("b")
+
+            defs = { "a": a, "b": b }
+            v, _ = Vote.objects.get_or_create(voting_id=vid, voter_id=uid,
                                           defaults=defs)
+        
         v.a = a
         v.b = b
 
         v.save()
 
         return  Response({})
+        
