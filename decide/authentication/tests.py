f3eee5f80fe5e96aace823b055cf0982360d4e00
nicsanmen <nicsanmen@alum.us.es>
2023-12-17 16:51:30 +0100

    Codacy

--- decide/authentication/tests.py
+++ decide/authentication/tests.py
@@ -14,9 +14,7 @@ from base.tests import BaseTestCase
 import time
 
 from selenium import webdriver
-from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.keys import Keys
 
 class AuthTestCase(APITestCase):
